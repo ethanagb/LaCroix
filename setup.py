198851d8957276bb4b108efcb164652dbb7ef335
Ethan A. G. Baker <ethanagbaker@gmail.com>
2019-07-02 15:29:22 -0400

    0.0.2: fixed install error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lacroix',
-      version='0.0.1',
+      version='0.0.2',
       description='Use LaCroix Color Palettes in Matplotlib & Seaborn',
       url='https://github.com/ethanagbaker/LaCroix',
       author='Ethan Alexander Garcia Baker',
