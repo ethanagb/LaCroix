b16557b7c0abc0239cbae1552f3b2b6e0a3b2c61
Ethan A. G. Baker <ethanagbaker@gmail.com>
2019-07-02 15:46:21 -0400

    v 0.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='lacroix',
-      version='0.0.2',
+      version='0.0.3',
       description='Use LaCroix Color Palettes in Matplotlib & Seaborn',
       url='https://github.com/ethanagbaker/LaCroix',
       author='Ethan Alexander Garcia Baker',
       author_email='ethanagbaker@gmail.com',
       license='cc-by-sa',
       packages=['lacroix'],
+      include_package_data=True,
       install_requires=['matplotlib'],
       zip_safe=False)
